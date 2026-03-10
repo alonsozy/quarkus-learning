683e5030542f390da90709f21dd7c247e820c370
alonsoz.y <alonsofisi@gmail.com>
2026-03-10 13:10:33 -0500

    02-Pushing initial changes

---
+++ 02.1-expense-service/src/main/java/com/bcp/training/ExpenseService.java
@@ -0,0 +1,50 @@
+package com.bcp.training;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Path;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.UUID;
+
+@ApplicationScoped
+public class ExpenseService {
+    private Set<Expense> expenses = Collections.newSetFromMap(Collections.synchronizedMap(new HashMap<>()));
+
+
+    @PostConstruct
+    public void init(){
+        expenses.add(
+                new Expense("Quarkus for Spring developers",Expense.PaymentMethod.DEBIT_CARD,"10.00")
+        );
+
+        expenses.add(
+                new Expense("Openshift for developers, Second Edition",Expense.PaymentMethod.CREDIT_CARD,"30.00")
+        );
+    }
+
+    public Set<Expense> list() {
+        return expenses;
+    }
+
+    public Expense create(Expense expense) {
+        expenses.add(expense);
+        return expense;
+    }
+
+    public boolean delete(UUID uuid) {
+        return expenses.removeIf(expense -> expense.getUuid().equals(uuid));
+    }
+
+    public void update(Expense expense) {
+        delete(expense.getUuid());
+        create(expense);
+    }
+
+    public boolean exists(UUID uuid) {
+        return expenses.stream().anyMatch(exp -> exp.getUuid().equals(uuid));
+    }
+}
